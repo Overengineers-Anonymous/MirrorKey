d6571a2304b57640894bc1f73984d4ee72e9a46c
rippleFCL <github@ripple.contact>
2025-06-06 13:58:27 +0100

    chore: moved to new repp

--- mirror_key/core/config/structure.py
+++
@@ -1,8 +0,0 @@
-from typing import Any
-from pydantic import BaseModel
-
-class ApiDescriptor(BaseModel):
-    config: Any
-
-class Config(BaseModel):
-    api_descriptors: dict[str, ApiDescriptor | None] | None
--- mirror_key/main.py
+++
@@ -1,9 +0,0 @@
-from core.api import route_map
-from core.plugins import plugin_loader
-from core.config import config
-
-config.load_config("mirrorkey.yml")
-plugin_loader.load()
-print(plugin_loader.loaded_plugins)
-
-# app = route_map.make_app()
